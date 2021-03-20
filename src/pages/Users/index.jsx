e42cab114bfca9c34ada0c3283ee830ed6558e47
tony <gwjlshebei@gmail.com>
2021-03-20 16:26:46 +0800

    分页器更新数据

--- src/pages/Users/index.jsx
+++ src/pages/Users/index.jsx
@@ -1,10 +1,12 @@
-import { Breadcrumb, Button, Card, Input, message, Space, Table, Switch } from "antd";
+import { Breadcrumb, Button, Card, Input, message, Space, Table, Switch, Form } from "antd";
 import React, { useState, useEffect, useRef, useCallback } from "react";
 import "./style.less";
 import http from "@/utils/http";
 import HomeLayout from "../Home";
 import { format } from "date-fns";
 import { CheckOutlined, EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import Modal from "antd/lib/modal/Modal";
+import { useForm } from "antd/lib/form/Form";
 
 const columns = [
   {
@@ -75,31 +77,34 @@ function Users() {
   });
   const paginationRef = useRef(pagination);
   paginationRef.current = pagination;
+  const queryRef = useRef(query);
+  queryRef.current = query;
 
-  const getUserList = useCallback(
-    async function () {
-      const newPagination = paginationRef.current;
-      const {
-        data,
-        meta: { msg, status },
-      } = await http.get("/users", {
-        params: {
-          query: query,
-          pagenum: newPagination.current,
-          pagesize: newPagination.pageSize,
-        },
-      });
-      if (status === 200) {
-        setDataSource(data.users);
-        setPagination({ ...newPagination, total: data.total });
-        message.success(msg);
-      } else {
-        message.error(msg);
-      }
-    },
+  const [visible, setVisible] = useState(false);
+  const [form] = useForm();
+  const getUserList = async function () {
+    const newPagination = paginationRef.current;
+    const query = queryRef.current;
+    const {
+      data,
+      meta: { msg, status },
+    } = await http.get("/users", {
+      params: {
+        query,
+        pagenum: newPagination.current,
+        pagesize: newPagination.pageSize,
+      },
+    });
 
-    [query]
-  );
+    console.log("data:", data);
+    if (status === 200) {
+      setDataSource(data.users);
+      setPagination({ ...newPagination, total: data.total });
+      message.success(msg);
+    } else {
+      message.error(msg);
+    }
+  };
   // 初始化数据
   /* eslint-disable */
   useEffect(() => {
@@ -108,10 +113,33 @@ function Users() {
 
   const handleChange = function (newPagination) {
     //更新pagination的值
-    paginationRef.current = { ...pagination, ...newPagination };
+    paginationRef.current = { ...paginationRef.current, ...newPagination };
     getUserList();
   };
 
+  const handleSearch = function (value) {
+    queryRef.current = value;
+    getUserList();
+  };
+
+  const handleAddUserOk = function () {
+    form.validateFields(async (err, values) => {
+      if (!err) {
+        const {
+          meta: { msg, status },
+        } = await http.post("/users", values);
+        // 提示成功，关闭弹出框，清除表单数据，更新数据(调用getUserList)
+        if (status === 201) {
+          message.success(msg);
+          getUserList();
+          form.resetFields();
+          setVisible(false);
+        } else {
+          message.error(msg);
+        }
+      }
+    });
+  };
   return (
     <HomeLayout>
       <div className="user-container">
@@ -123,10 +151,34 @@ function Users() {
               <Breadcrumb.Item>用户列表</Breadcrumb.Item>
             </Breadcrumb>
             <Space key="search">
-              <Input.Search enterButton />
-              <Button>添加用户</Button>
+              <Input.Search enterButton onSearch={handleSearch} />
+              <Button onClick={() => setVisible(true)}>添加用户</Button>
             </Space>
-            {JSON.stringify(dataSource)}
+            <Modal
+              title="添加用户"
+              okText="确定"
+              cancelText="取消"
+              onOk={handleAddUserOk}
+              onCancel={() => {
+                setVisible(false);
+              }}
+              visible={visible}
+            >
+              <Form form={form}>
+                <Form.Item label="用户名" name="username" rules={[{ required: true, message: "请输入用户名!" }]}>
+                  <Input />
+                </Form.Item>
+                <Form.Item label="密码" name="password" rules={[{ required: true, message: "请输入密码!" }]}>
+                  <Input.Password />
+                </Form.Item>
+                <Form.Item label="邮箱" name="email">
+                  <Input />
+                </Form.Item>
+                <Form.Item label="电话" name="mobile">
+                  <Input />
+                </Form.Item>
+              </Form>
+            </Modal>
             <Table columns={columns} dataSource={dataSource} pagination={pagination} onChange={handleChange} />
           </Space>
         </Card>
